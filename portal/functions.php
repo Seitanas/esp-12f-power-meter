a0eb2b080e0a7a1cdb85459bb2cb29e55c179b95
Seitanas <tadas@ring.lt>
2017-05-09 08:25:45 +0300

    Fixing mysql set names function

--- portal/functions.php
+++ portal/functions.php
@@ -2,7 +2,7 @@
 function SQL_connect(){
     include (dirname(__FILE__).'/config.php');
     $mysql_connection=mysqli_connect($mysql_host,$mysql_user,$mysql_pass);
-    $mysql_connection->set_charset('utf8');
+    mysqli_set_charset($mysql_connection, 'utf8');
     mysqli_select_db($mysql_connection, $mysql_db);
     return $mysql_connection;
 }
